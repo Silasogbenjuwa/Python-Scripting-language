fbe7e0104ea37be73fa0e0fb2d446e43954d3526
Silasogbenjuwa <Silasogbogb@outlook.com>
2025-02-06 14:14:20 -0800

    Workshop 2 initial commit

---
+++ workshop2.py
@@ -0,0 +1,27 @@
+#Workshop 2: 6th Feb 2025
+
+#1. Write a program that Calculates the product of three numbers
+a = 5
+b = 3
+c = 4
+product = a * b * c
+print(product) #Output: 60
+
+
+#2. Given a length of 8cm and a width of 3cm. Write a program that calculates the area of the rectangle
+length = 8
+width = 3
+area = length * width
+print(area) #Output: 24 cm**2
+
+
+#3. Write a program that stores your name in a string called 'name' and prints 'Greatings name'
+name = "Silas"
+print("Greatings", name) #Output:Greatings Silas
+
+
+#4. Write a program that calculates the average of two numbers
+a = 55
+b = 65
+average = (a + b)/2
+print(average) #Output: 60.0
